f6c523968c8ee1d923ff5ff1e828d50e7b75ce54
徐文献 <sam2227219@gmail.com>
2024-08-02 03:16:46 +0000

    delete

--- client-server/client.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-request('https://yakimhsu.com',
-  function (error, response, body) {
-    console.error('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('head:', response.headers); // 多印 response 的 header
-    console.log('body:', body);
-});
