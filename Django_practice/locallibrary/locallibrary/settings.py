ad855969a1d50c4be625a7b5a95164fe531f9702
徐文献 <sam2227219@gmail.com>
2024-08-05 01:35:46 +0000

    setting change

--- Django_practice/locallibrary/locallibrary/settings.py
+++ Django_practice/locallibrary/locallibrary/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dw@s=5$yn+h*zp2rvdlnn6cdxk)4$pxy!8=edbx-o(-3lti4ny'
+#SECRET_KEY = 'django-insecure-dw@s=5$yn+h*zp2rvdlnn6cdxk)4$pxy!8=edbx-o(-3lti4ny'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dw@s=5$yn+h*zp2rvdlnn6cdxk)4$pxy!8=edbx-o(-3lti4ny')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
