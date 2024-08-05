f655aebee14b1f29db2ace669a0e34c418083710
徐文献 <sam2227219@gmail.com>
2024-08-05 01:38:11 +0000

    setting change

--- Django_practice/locallibrary/locallibrary/settings.py
+++ Django_practice/locallibrary/locallibrary/settings.py
@@ -146,13 +146,4 @@ if 'DATABASE_URL' in os.environ:
     )
 
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+
